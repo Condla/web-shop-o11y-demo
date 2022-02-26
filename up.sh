4eb24bebe9fd7bfa7db1fec19592d43a7477bbe7
Stefan Dunkler <stefan.dun@gmail.com>
2022-02-26 21:52:40 +0000

    add metrics and traces collection to spring boot service

---
+++ up.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+wget -O opentelemetry-javaagent.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
+docker-compose up -d --build
