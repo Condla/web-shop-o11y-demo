cb1f8c165afaf27d824391a4f15dde66456caa1e
Stefan Dunkler <stefan.dun@gmail.com>
2022-06-02 18:03:05 +0000

    bake opentelemetry-javaagent.jar into container

--- up.sh
+++ up.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions && sudo systemctl restart docker
 docker plugin ls
-wget -O opentelemetry-javaagent.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
 docker-compose up -d
