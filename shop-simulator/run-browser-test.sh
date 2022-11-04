fba1a5e0142acd803205ca11d7155d5123b81816
Stefan Dunkler <stefan.dun@gmail.com>
2022-11-04 16:36:41 +0000

    add browser test script

---
+++ shop-simulator/run-browser-test.sh
@@ -0,0 +1,60 @@
+import { chromium } from 'k6/x/browser';
+import exec from 'k6/execution';
+import { sleep } from 'k6';
+import { check } from 'k6';
+
+export default function () {
+  const browser = chromium.launch({
+    headless: false,
+    slowMo: '500ms',
+  });
+
+  const context = browser.newContext({
+        screen: {width: 1024, height: 768},
+        viewport: { width: 1024, height: 768}
+  });
+  const page = context.newPage();
+  page.goto('http://34.79.93.8:3389/shop?name=Emil');
+  
+  page.mouse.move(344, 402)
+  sleep(1); 
+  page.mouse.move(530, 410) 
+  
+  page.mouse.click(344, 402)
+  sleep(1) 
+  page.mouse.click(530, 410) 
+  
+  sleep(1);
+
+  let selector, elem;
+  
+  selector='[href="/cart?name=Emil"]';
+  elem = page.$(selector);
+  elem.click();
+
+  sleep(1);
+  
+  selector='.w3-main';
+  elem = page.$(selector);
+  
+  let re= /Meows\s*(.*)\s*(.*)\s*(.*)\s*Loki\s*(.*)\s*(.*)\s*(.*)\s*/;
+  const match = elem.textContent().match(re);
+  
+  let total= Number(match[3])+Number(match[6]);
+  console.log("Total: "+total);
+  
+  check(page, { in: "69.98"== total});
+
+  sleep(1);
+  
+  page.screenshot({ path:"kitties.png" });
+  
+  selector='[value="Emil"]';
+  elem = page.$(selector);
+  elem.click();
+
+  sleep(1);
+
+  page.close();
+  browser.close();
+}
