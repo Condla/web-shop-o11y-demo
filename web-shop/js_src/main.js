f88f5109f7ef37962a7f41ce2862bcd3f32188f4
Stefan Dunkler <stefan.dun@gmail.com>
2022-11-10 18:44:06 +0000

    add front end browser monitoring

--- web-shop/js_src/main.js
+++ web-shop/js_src/main.js
@@ -10,12 +10,11 @@ const faro = initializeFaro({
   apiKey: 'secret',
   instrumentations: [
     ...getWebInstrumentations(),
-    new TracingInstrumentation(),
-
     new TracingInstrumentation({
       // Optional, if you want to add custom attributes to the resource
       resourceAttributes: {
         "service.name": "web-shop-frontend",
+        "team.name": "front-end"
       },
     })
 
@@ -25,6 +24,7 @@ const faro = initializeFaro({
     version: '1.4.0',
   },
 });
+
 faro.api.pushLog(['Hello, Faro!']);
 
 //export const {trace, context} = faro.api.getOTEL();
@@ -51,16 +51,74 @@ function w3_open() {
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
-
-    // send a log message
-    faro.api.pushLog(['hello faro!']);
-    // will be captured
-  //  throw new Error('oh no');
-
-    // push error manually
-    faro.api.pushError(new Error('oh no'));
   }
+
+function buy_now(product_name, person_name){
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/shop?product=" + product_name + "&name=" + person_name, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    "product_name": product_name,
+    "person": person_name
+  }));
+  //window.alert('Successfully added item to shopping cart. Add more items or pay the shopping cart (right upper corner)')
+}
+
+function empty_cart(person_name){
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/cart?name=" + person_name, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    "person": person_name
+  }));
+  window.alert('shopping cart emptied');
+  location.reload();
+}
+
+function checkout_cart(person_name){
+  var xhr = new XMLHttpRequest();
+  var rand = Math.random();
+  if (rand < 0.7){
+      xhr.open("POST", "/cart?name=" + person_name + "&checkout=true", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+        "person": person_name,
+        "checkout": true
+      }));
+      console.log("Successfully checked out.");
+      window.alert("Successfully checked out. Please come again and buy some more stuff.");
+  }
+  else if (rand < 0.8) {
+      faro.api.pushError(new Error("ERROR CODE 9999: Mainframe on fire!"));
+      window.alert("ERROR CODE 999: Mainframe on fire! Please try again in a few seconds.");
+  }
+  else if (rand < 0.9) {
+      faro.api.pushError(new Error("ERROR CODE 2023: Happy new year!"));
+      window.alert("ERROR CODE 2023: Happy new year! Please try again in a few seconds.");
+  }
+  else if (rand <= 1) {
+      faro.api.pushError(new Error("ERROR CODE 2412: Feliz Navidad."));
+      window.alert("ERROR CODE 24.12: Feliz Navidad. Please try again in a few seconds.");
+  }
+      location.reload();
+}
+
+function apply_discount(person_name){
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/cart?name=" + person_name + "&discount=true", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    "person": person_name,
+    "discount": true
+  }));
+  window.alert("You're too cheap to be eligible for a discount! Please go to amazon to buy your stuff.");
+  location.reload();
+}
 
   window.myAccFunc = myAccFunc;
   window.w3_open = w3_open;
   window.w3_close = w3_close;
+  window.buy_now = buy_now;
+  window.empty_cart = empty_cart;
+  window.checkout_cart = checkout_cart;
+  window.apply_discount = apply_discount;
--- web-shop/routes.py
+++ web-shop/routes.py
@@ -1,7 +1,6 @@
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 import requests
 from flask import current_app as app
-from os import environ
 
 shopping_cart_url = "http://shopping-cart:5555"
 products_url = "http://products:8080"
@@ -40,7 +39,7 @@ def view_shop():
     
     products = get_products(request_string)
 
-    if request.method == "POST":
+    if request.method == "POST" and product_name:
         add_to_shopping_cart(person, product_name, headers)
         
     app.logger.info("Showing web interface.") 
@@ -101,6 +100,17 @@ def get_items_from_shopping_cart(request_string):
         items = response.json()
         app.logger.info("Successfully obtained items from shopping cart")
     return items
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('view_shop', name=request.form['username']))
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
