f37dfa86e373831c530ae8447c9b37703610925b
apham <alainphm@gmail.com>
2023-01-06 16:34:57 +0100

    deploy script fix and agent scrape web shop port

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -8,10 +8,10 @@ export TEMPO_URL=YOURURL
 export TEMPO_USR=YOURUSER
 export TEMPO_KEY=YOURKEY
 
-kubectl apply -f kubernetes/fullstack-with-grafana-cloud/01-namespace.yaml
+kubectl apply -f kubernetes/01-namespace.yaml
 
-envsubst < kubernetes/fullstack-with-grafana-cloud/02-agent.yaml | kubectl apply -f -
+envsubst < kubernetes/02-agent.yaml | kubectl apply -f -
 
 export APP_RECEIVER=$(kubectl get service -n web-shop-app grafana-agent-external  -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-envsubst < kubernetes/fullstack-with-grafana-cloud/03-web-shop.yaml | kubectl apply -f -
+envsubst < kubernetes/03-web-shop.yaml | kubectl apply -f -
