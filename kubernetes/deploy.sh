142f8bdab72a69407d95df30e4d6a88ff1a32aba
apham <alainphm@gmail.com>
2023-01-22 18:32:51 +0100

    adeploy and agent

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -16,4 +16,8 @@ envsubst < kubernetes/02-agent.yaml | kubectl apply -f -
 
 export APP_RECEIVER=$(kubectl get service -n web-shop-app grafana-agent-external  -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
+echo $APP_RECEIVER
+
 envsubst < kubernetes/03-web-shop.yaml | kubectl apply -f -
+
+envsubst < kubernetes/04-xk6-browser.yaml | kubectl apply -f -
