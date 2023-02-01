f79238a70423125ee592a3bf7ca33286c00340e2
Stefan Dunkler <stefan.dun@gmail.com>
2023-02-01 08:29:53 +0000

    add --build flag but commented out

--- up.sh
+++ up.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
 # insert your public IP or URL here
-#export PUBLIC_APP_URL=${PUBLIC_APP_URL:=grafana.datahovel.com}
-export PUBLIC_APP_URL=${PUBLIC_APP_URL:=localhost}
+export PUBLIC_APP_URL=${PUBLIC_APP_URL:=grafana.datahovel.com}
+#export PUBLIC_APP_URL=${PUBLIC_APP_URL:=localhost}
 
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions && sudo systemctl restart docker
 docker plugin ls
+#docker-compose up -d --build
 docker-compose up -d
