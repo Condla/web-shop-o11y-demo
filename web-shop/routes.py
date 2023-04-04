1654aff8b2e415c4188cf18320a26288de55494b
Stefan Dunkler <stefan.dun@gmail.com>
2023-04-04 19:32:45 +0000

    fix some order id related bugs and improve dashboards

--- web-shop/routes.py
+++ web-shop/routes.py
@@ -68,36 +68,30 @@ def get_products(request_string):
 
 def check_out_cart(checkout, headers, request_string, person):
     items = get_items_from_shopping_cart(request_string)
+    if checkout:
+        order_uuid = uuid.uuid4()
+        order_request_string = "{}/order/{}".format(shopping_cart_url, order_uuid)
+        payload = json.loads('{"name": "' + person + '"}')
+        app.logger.debug("Trying to create order {}".format(order_uuid))
+        response = requests.post(order_request_string, json=payload, headers=headers, proxies=proxies)
+        if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
+            app.logger.exception("Could not create order {} ".format(order_uuid))
+        else:
+            app.logger.info("Created order {}.".format(order_uuid))
+
+        for item in items:
+            product_request_string = "{}/products/checkout".format(products_url)
+            payload = item
+            response = requests.post(product_request_string, json=payload, headers=headers, proxies=proxies)
+            if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
+              app.logger.exception("Could not check out item.")
+            else:
+              app.logger.info("Checked out item.")
     response = requests.delete(request_string, headers=headers, proxies=proxies)
     if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
         app.logger.exception("Got a real bad response from shopping cart. Something is wrong.")
     else:
         app.logger.info("Successfully emptied shopping cart.")
-        if checkout:
-            if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
-               app.logger.exception("Got a real bad response from shopping cart. Something is wrong.")
-            else:
-# create unique id for each order
-# call the method "{}/create_order".format(shopping_cart_url
-#payload ="        request_string = "{}/cart/{}".format(shopping_cart_url, person)
-                order_uuid = uuid.uuid4()
-                order_request_string = "{}/order/{}".format(shopping_cart_url, order_uuid)
-                payload = json.loads('{"name": "' + person + '"}')
-                app.logger.debug("Trying to create order {}".format(order_uuid))
-                response = requests.post(order_request_string, json=payload, headers=headers, proxies=proxies)
-                if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
-                    app.logger.exception("Could not create order {} ".format(order_uuid))
-                else:
-                    app.logger.info("Created order {}.".format(order_uuid))
-
-                for item in items:
-                    request_string = "{}/products/checkout".format(products_url)
-                    payload = item
-                    response = requests.post(request_string, json=payload, headers=headers, proxies=proxies)
-                    if not (response.status_code == 200 or response.status_code == 201 or response.status_code == 202):
-                      app.logger.exception("Could not check out item.")
-                    else:
-                      app.logger.info("Checked out item.")
 
 def apply_discount(person, headers, request_string):
     request_string_discount = request_string + "/discount"
